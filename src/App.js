b37e6523f3fd442b0c21d2ddbfb65667779952b4
RYOTAN9688 <ryota8813@gmail.com>
2021-07-21 00:33:32 +0900

    天気アプリ

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Weather from "./weater";
+const App = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
